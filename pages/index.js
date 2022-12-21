9c6dbea0881bfa7300b2dc9586c49aaf84669439
OlacodesLA <zaccheausjide@gmail.com>
2022-12-21 15:02:38 +0100

    second comit

--- pages/index.js
+++ pages/index.js
@@ -414,7 +414,6 @@ const Home = () => {
               type="submit"
               className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
               onClick={getDateTime}
-              disabled={submit ? false : true}
             >
               Submit
             </button>
